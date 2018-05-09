60f7a9befc1b2515b4567b668b349c474830bde7
Ramsey Khadder <ramseykhadder@hotmail.com>
2018-05-08 18:19:17 -0700

    Starter Project

---
+++ Starter Project/Assets/ConnectedStorage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConnectedStorage : MonoBehaviour
+{
+    public Text StoredValueText;
+    public Text RandomValueText;
+    
+    int randomValue;
+    
+    public void Init()
+    {
+    }
+
+    public void GenerateRandomValue()
+    {
+        randomValue = Random.Range(0, 100);
+        RandomValueText.text = randomValue.ToString();
+    }
+
+    public void Save()
+    {
+    }
+
+    public void Load()
+    {
+    }
+}
